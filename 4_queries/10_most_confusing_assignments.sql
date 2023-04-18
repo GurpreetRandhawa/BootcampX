22a57bd75d7f258fd80bb1c31d9bf22341293cfd
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-04-18 00:30:12 -0700

    Add queries optimisation

--- 4_queries/10_most_confusing_assignments.sql
+++ 4_queries/10_most_confusing_assignments.sql
@@ -1,5 +1,7 @@
-SELECT assignments.id AS id , count(assistance_requests.*) AS total_requests, assignments.name AS name, assignments.day AS day, assignments.chapter AS chapter
+SELECT assignments.id AS id , assignments.name AS name, assignments.day AS day, assignments.chapter AS chapter, count(assistance_requests.*) AS total_requests
 FROM assistance_requests 
 JOIN assignments ON assignments.id = assignment_id
 GROUP BY assignments.id
 ORDER BY total_requests DESC;
+
+

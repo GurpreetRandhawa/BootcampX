d104b6136714543580af2b7d33cc1032a8b15e6d
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-05-31 15:30:14 -0700

    Add parametrized query to prevent sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -23,16 +23,18 @@ let limit_input = process.argv[3];
 //       );
 //     });
 //   })
-pool
-  .query(
-    `
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const limit = process.argv[3] || 5;
+const values = [`%${process.argv[2]}%`, limit];
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
